b829026c832ae62597658c6e34fd1cf1fc1547ee
yashbaldev <113610963+yashbaldev@users.noreply.github.com>
2023-12-16 20:12:57 +0530

    upload the files of the chrome extension

---
+++ script.js
@@ -0,0 +1,24 @@
+async function getMatchData(){
+    
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=b96e9622-cb41-4348-90c4-31b5deaaf3ad&offset=0")
+       .then(data=>data.json())
+       .then(data=>{
+        if(data.status != "success") return;
+        
+        const matchesList = data.data;
+
+        if(!matchesList)return [];
+
+        const relevantData = matchesList.map(match =>`${match.name},${match.status}`);
+
+        console.log(relevantData);
+        
+        document.getElementById("matches").innerHTML = relevantData.map(match => `<li> ${match} </li>`).join("");
+
+        return relevantData;
+       })
+       .catch(err =>(console.log(err)));
+
+}
+
+getMatchData();
